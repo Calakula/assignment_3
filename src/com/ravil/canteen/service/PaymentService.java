005d284bbbde66f2ddcbf2f96bcfb91e294d533b
zhaksylykaruzhan-commits <zhaksylyk.aruzhan.alash@gmail.com>
2026-01-29 19:19:33 +0500

    Update PaymentService.java

--- src/com/ravil/canteen/service/PaymentService.java
+++ src/com/ravil/canteen/service/PaymentService.java
@@ -1,4 +1,15 @@
 package com.ravil.canteen.service;
 
+import com.ravil.canteen.model.OrderItem;
+
+import java.math.BigDecimal;
+import java.util.List;
+
 public class PaymentService {
-}
+    public BigDecimal calculateTotal(List&lt;OrderItem&gt; items) {
+        BigDecimal total = BigDecimal.ZERO;
+        for (OrderItem oi : items) {
+            total = total.add(oi.getUnitPrice().multiply(BigDecimal.valueOf(oi.getQuantity())));
+        }
+        return total;
+    }
