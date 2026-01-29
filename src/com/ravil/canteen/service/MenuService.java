f5b7f534e3f7394d891349c4f7dd0ec4cb295dbc
zhaksylykaruzhan-commits <zhaksylyk.aruzhan.alash@gmail.com>
2026-01-29 19:16:24 +0500

    Update MenuService.java

--- src/com/ravil/canteen/service/MenuService.java
+++ src/com/ravil/canteen/service/MenuService.java
@@ -1,4 +1,27 @@
 package com.ravil.canteen.service;
 
+import com.ravil.canteen.model.MenuItem;
+import com.ravil.canteen.repository.MenuItemRepository;
+
+import java.util.List;
+
 public class MenuService {
+    private final MenuItemRepository menuRepo;
+
+    public MenuService(MenuItemRepository menuRepo) {
+        this.menuRepo = menuRepo;
+    }
+
+    public List<MenuItem> listAvailable() {
+        return menuRepo.findAllAvailable();
+    }
+
+    public MenuItem getAvailableItem(int id) {
+        return menuRepo.findById(id)
+                .filter(MenuItem::isAvailable)
+                .orElse(null);
+    }
 }
+
+
+
