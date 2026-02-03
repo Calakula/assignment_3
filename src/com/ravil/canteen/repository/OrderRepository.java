297536cac44e1463b77d9dbcc61f65cab62a8386
zhaksylykaruzhan-commits <zhaksylyk.aruzhan.alash@gmail.com>
2026-02-03 08:26:40 +0500

    Ravil Maratov Updated OrderRepository

--- src/com/ravil/canteen/repository/OrderRepository.java
+++ src/com/ravil/canteen/repository/OrderRepository.java
@@ -6,6 +6,7 @@ import com.ravil.canteen.util.Db;
 import java.sql.*;
 import java.util.*;
 
+
 public class OrderRepository {
     public Order createActive(int customerId) {
         String sql = "INSERT INTO orders (customer_id, status) VALUES (?, 'ACTIVE') RETURNING id, created_at";
