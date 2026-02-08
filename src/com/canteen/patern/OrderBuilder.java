bb489880e627d0b000bec9f4b53da186fa9a3203
Aruzhan <zhaksylyk.aruzhan.alash@gmail.com>
2026-02-08 23:07:31 +0500

    Aruzhan Zhakslyk OrderBuilder.java

--- src/com/canteen/patern/OrderBuilder.java
+++ src/com/canteen/patern/OrderBuilder.java
@@ -1,4 +1,47 @@
-package com.canteen.patern;
+package com.canteen.pattern.builder;
+
+import com.canteen.entity.Order;
+import com.canteen.entity.OrderItem;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 public class OrderBuilder {
+    private int id;
+    private int customerId;
+    private String status;
+    private LocalDateTime createdAt;
+    private List<OrderItem> items = new ArrayList<>();
+
+    public OrderBuilder setId(int id) {
+        this.id = id;
+        return this;
+    }
+
+    public OrderBuilder setCustomerId(int customerId) {
+        this.customerId = customerId;
+        return this;
+    }
+
+    public OrderBuilder setStatus(String status) {
+        this.status = status;
+        return this;
+    }
+
+    public OrderBuilder setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+        return this;
+    }
+
+    public OrderBuilder addItem(OrderItem item) {
+        this.items.add(item);
+        return this;
+    }
+
+    public Order build() {
+        Order order = new Order(id, customerId, status, createdAt);
+        order.setItems(items);
+        return order;
+    }
 }
