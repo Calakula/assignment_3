2d005a4db7beb723acd7c56e10a4350e0fbd46b5
Aruzhan <zhaksylyk.aruzhan.alash@gmail.com>
2026-02-08 23:13:36 +0500

    Aruzhan Zhaksylyk add DeliveryFactory

--- src/com/canteen/patern/factory/DeliveryFactory.java
+++ src/com/canteen/patern/factory/DeliveryFactory.java
@@ -1,4 +1,12 @@
-package com.canteen.patern.factory;
+package com.canteen.pattern.factory;
 
 public class DeliveryFactory {
+    public static DeliveryOption createDelivery(String type) {
+        return switch (type.toLowerCase()) {
+            case "pickup" -> new PickupOrder();
+            case "delivery" -> new DeliveryOrder();
+            case "dinein" -> new DineInOrder();
+            default -> throw new IllegalArgumentException("Unknown delivery type: " + type);
+        };
+    }
 }
