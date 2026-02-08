398da76830a6c5ad9c42ed65d06a68021fb9c396
Aruzhan <zhaksylyk.aruzhan.alash@gmail.com>
2026-02-08 20:45:58 +0500

    Aruzhan Zhaksylyk add TaxConfig.java

--- src/com/canteen/patern/TaxConfig.java
+++ src/com/canteen/patern/TaxConfig.java
@@ -1,4 +1,20 @@
 package com.canteen.patern;
 
-public class TaxConfig {
+public class TaxConfig {  
+  private static TaxConfig instance;
+    private double taxRate = 0.15;
+    private TaxConfig() {}
+
+    public static TaxConfig getInstance() {
+        if (instance == null) {
+            instance = new TaxConfig();
+        }
+        return instance;
+    }
+    public double calculateTotalWithTax(double subtotal) {
+        return subtotal * 1.15;
+    }
+
+    public double getTaxRate() { return taxRate; }
+    public void setTaxRate(double rate) { this.taxRate = rate; }
 }
