651dd8eee5523464f071759c94800afd4a916ccb
Calakula <ravilkuanov1407@gmail.com>
2026-02-10 08:36:40 +0500

    Ravil Maratov Hide database credentials in Db.java

--- src/com/canteen/util/Db.java
+++ src/com/canteen/util/Db.java
@@ -7,8 +7,8 @@ import java.sql.SQLException;
 
 public class Db {
     private static final String URL = "jdbc:postgresql://aws-1-ap-southeast-1.pooler.supabase.com:5432/postgres?sslmode=require";
-    private static final String USER = "postgres.uvmffdyfyxpnomfzbnyx";
-    private static final String PASSWORD = "Ravil1407@@";
+    private static final String USER = "***";
+    private static final String PASSWORD = "***";
 
     public static Connection getConnection() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASSWORD);
